587c2e82f10a84d47a424d47f43df21559f28362
Kashviisharma <kashvi.sharma.eng@gmail.com>
2026-07-17 14:44:50 +0530

    created filter map reduce in js

---
+++ ex2.js
@@ -0,0 +1,11 @@
+const a=[1,2,3,4,5,6,7,8,9];
+const b=a.filter((i)=>(i%2==0));
+console.log("Even=",b);
+
+//create a squared array
+const c=b.map((i)=>i*i);
+console.log("Square=",c);
+//Sum of all squared no.s
+
+const d=c.reduce((i,s)=>s+i);
+console.log("Sum of squares=",d);
