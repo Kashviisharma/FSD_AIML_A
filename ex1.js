e8f46c41aaa4130f6fb9fcf092204f9283dea61b
Kashviisharma <kashvi.sharma.eng@gmail.com>
2026-07-17 14:20:14 +0530

    Created ex1.js file

---
+++ ex1.js
@@ -0,0 +1,17 @@
+// var a=10;
+// var a=20;
+// console.log("a=",a);
+// var a=30;
+// console.log(a*3);
+// function sum(a,b){
+//     return a+b;
+// }
+
+// console.log("sum=",sum(10,20));
+
+//Arrow Function
+const sum=(a,b)=>a+b;
+console.log("sum=",sum(10,80));
+console.log("sum=",sum(70,80));
+console.log("sum=",sum(10,75));
+
